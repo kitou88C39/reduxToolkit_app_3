e60e198787679b905e1a162ab09e5051b94ddffd
kitou88C39 <kanfutrooper@icloud.com>
2022-02-18 09:46:06 +0900

    fourth commit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+//ログインユーザーが自身の口座に入出金する
+export const counterSlice = createSlice({
+  name: "currentUser",
+  initialState: {
+    count: 0,
+  },
+  reducers: {
+    increase: (state) => {
+      state.count += num;
+    },
+    decrease: (state) => {
+      state.count -= num;
+    },
+  },
+});
+
 const initialState = {
   idCount: 1,
   todos: [{ id: 1, name: "addTodos", money: "balance" }],
@@ -8,6 +24,7 @@ const initialState = {
 const addTodoReducer = createSlice({
   name: "todos",
   initialState,
+  balance: 0,
   reducers: {
     //ここでは、reducerを書きます。
     //todoを追加
@@ -25,22 +42,23 @@ const addTodoReducer = createSlice({
       };
     },
     //todoを更新
-    updateTodos: (state, action) => {
-      return {
-        ...state,
-        todos: state?.todos?.map((todo) => {
-          if (todo.id === action.payload.id) {
-            return {
-              ...todo,
-              balance: action.payload.balance,
-            };
-          }
-          return todo;
-        }),
-      };
-    },
+    // updateTodos: (state, action) => {
+    //   return {
+    //     ...state,
+    //     todos: state?.todos?.map((todo) => {
+    //       if (todo.id === action.payload.id) {
+    //         return {
+    //           ...todo,
+    //           balance: action.payload.balance,
+    //         };
+    //       }
+    //       return todo;
+    //     }),
+    // };
+    // },
   },
 });
 
+export const { increase, decrease } = counterSlice.actions;
 export const { addTodos, removeTodos, updateTodos } = addTodoReducer.actions;
 export const reducer = addTodoReducer.reducer;
