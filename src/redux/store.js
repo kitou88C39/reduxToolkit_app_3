5bfa2a2f055cd0018cf5b024c39269e74c4b5490
kitou88C39 <kanfutrooper@icloud.com>
2022-04-09 20:20:07 +0900

    twenty seven commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 // some function
-import { senderReducer, reducer } from "./reducer";
+import { reducer } from "./reducer";
 
 const store = configureStore({
   //reducer: { senderReducer, reducer },
   // some function
-  senders: senderReducer,
-  todos: reducer,
+  reducer: {
+    senders: reducer,
+    todos: reducer,
+  },
 });
 export default store;
